18daaa70496c8569b528cc939eb6dd3bfdccef8c
Julian Wesch <julian.wesch@gmail.com>
2022-01-06 11:30:26 +0100

    redis introduced

--- batterymeter.py
+++ batterymeter.py
@@ -1,21 +1,22 @@
 import time
 import Adafruit_ADS1x15
-
+import redis
+import struct
 # yellow SDA
 # orange SCL
-
+r = redis.Redis(host='localhost', port=6379, db=0)
 adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
 GAIN = 2/3
 VoltageLastMeasured=0
-VoltageMeasuringPeriod=1
+VoltageMeasuringPeriod=5
 while True:
     if time.time()-VoltageLastMeasured>VoltageMeasuringPeriod:
         read1=adc.read_adc(1, gain=GAIN) * 2
         read3=adc.read_adc(3, gain=GAIN)
         cell1 = (read1-read3)/32768*6.144
         cell2 = (read3)/32768*6.144
+
         VoltageLastMeasured=time.time()
-        print("Cell 1: ", round(cell1,1),"V")
-        print("Cell 2: ", round(cell2,1),"V")
-
-
+        voltages_bytes = struct.pack('%sf' %2,* [cell1, cell2])
+        print(struct.unpack('%sf' %2,voltages_bytes))
+        r.set('voltages',voltages_bytes)
